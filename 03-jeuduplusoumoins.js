5c8f46b438ae0cb63e4b1684310abe4a0d6a1f24
junglemove <thibaut.visticot@gmail.com>
2016-12-12 13:14:02 +0100

    03: my solution

--- 03-jeuduplusoumoins.js
+++ 03-jeuduplusoumoins.js
@@ -3,11 +3,11 @@
  */
 const readline = require('readline');
 
-let random = function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+const random = function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 };
 
-const randomNumber = Math.floor(random(0,100));
+const randomNumber = random(0,100);
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -15,19 +15,21 @@ const rl = readline.createInterface({
 });
 
 const play = function play(){
-    rl.question('What do you think of Node.js? ', function(answer) {
-        // TODO: Log the answer in a database
-        console.log(`Thank you for your valuable feedback: ${answer}`);
+    rl.question('Enter a number? ', function(answer) {
+
+        if(isNaN(answer)){
+            throw Error(e.stack);
+        };
 
         if(answer ==  randomNumber){
-            console.log("Answer:" + answer + "=" + "Random:" + randomNumber);
+            console.log("Answer: " + answer + " = " + "Random: " + randomNumber);
             console.log("you win");
             rl.close();
         }else if(answer > randomNumber ){
-            console.log("Answer:" + answer + ">" + "Random:" + randomNumber);
+            console.log("Answer: " + answer + " > " + "Random: " + randomNumber);
             play();
         }else{
-            console.log("Answer:" + answer + "<" + "Random:" + randomNumber);
+            console.log("Answer: " + answer + " < " + "Random: " + randomNumber);
             play();
         }
     });
